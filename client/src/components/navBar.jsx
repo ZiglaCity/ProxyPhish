11f5a35cbff27c80d3d311ba34a83243fd1b628d
ZiglaCity <ziglacity@gmail.com>
2025-06-07 02:03:31 +0000

    Update

--- client/src/components/navBar.jsx
+++ client/src/components/navBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
     return (
         <nav className="w-full py-6 px-4 text-center border-b border-gray-700">
             <h1 className="text-3xl font-bold text-white flex justify-center items-center gap-2">
-                Phish<span className="text-green-700">Guard</span>
+                Proxy<span className="text-green-700">Phish</span>
                 <Shield className="text-green-700" size={32} />
             </h1>
             <p className="text-sm text-gray-400 mt-2">Check if a URL is legit or a trap!</p>
