f5ee4530e55315a65b0886ac8c11a3b076802a43
ZiglaCity <ziglacity@gmail.com>
2025-06-07 02:01:36 +0000

    Added navBar

---
+++ client/src/components/navBar.jsx
@@ -0,0 +1,15 @@
+import { Shield } from "lucide-react";
+
+const NavBar = () => {
+    return (
+        <nav className="w-full py-6 px-4 text-center border-b border-gray-700">
+            <h1 className="text-3xl font-bold text-white flex justify-center items-center gap-2">
+                Phish<span className="text-green-700">Guard</span>
+                <Shield className="text-green-700" size={32} />
+            </h1>
+            <p className="text-sm text-gray-400 mt-2">Check if a URL is legit or a trap!</p>
+        </nav>
+    );
+};
+
+export default NavBar;
