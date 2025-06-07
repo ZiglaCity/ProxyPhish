da24a14b988babc8c893a64e7fc58aa423786bb4
ZiglaCity <ziglacity@gmail.com>
2025-06-07 10:04:21 +0000

    About component

---
+++ client/src/components/UrlInput.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const UrlInput = () => {
+    return (
+        <div>
+            <input></input>
+        </div>
+    );
+};
+
+export default UrlInput;
--- client/src/components/navBar.jsx
+++ client/src/components/navBar.jsx
@@ -1,13 +1,15 @@
-import { Shield } from "lucide-react";
+import { Shield } from 'lucide-react';
 
 const NavBar = () => {
     return (
         <nav className="w-full py-6 px-4 text-center border-b border-gray-700">
-            <h1 className="text-3xl font-bold text-white flex justify-center items-center gap-2">
+            <h1 className="text-3xl font-bold text-white flex justify-center items-center">
                 Proxy<span className="text-green-700">Phish</span>
-                <Shield className="text-green-700" size={32} />
+               <Shield className="text-green-700" size={32} />
             </h1>
-            <p className="text-sm text-gray-400 mt-2">Check if a URL is legit or a trap!</p>
+            <p className="text-sm text-gray-400 mt-2">
+                Check if a URL is legit or a trap!
+            </p>
         </nav>
     );
 };
